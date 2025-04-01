a99d9b5531cbad4e450271ca4792d9fa63789740
Alexander <lebo8020@mylaurier.ca>
2025-04-01 18:22:59 -0400

    added guards

--- rules-aux.h
+++ rules-aux.h
@@ -1,4 +1,8 @@
+#ifndef RULES_AUX_H
+#define RULES_AUX_H
+
 #include "Apriori.h"
+#include "itemset_hash_table.h"
 
 ItemSet *generate_candidate(ItemSet *itemset1, ItemSet *itemset2, int k);
 int has_frequent_subsets(ItemSet *candidate, ItemSet *frequent_itemsets,
@@ -9,5 +13,8 @@ void print_rule(AssociationRule rule);
 float find_itemset_support_fixed(ItemSet *itemsets, int count, int *items,
                                  int size);
 AssociationRule *generate_rules_fixed(ItemSet *itemsets, int itemset_count,
-                                      float min_confidence, int *rule_count);
+                                      float min_confidence, int *rule_count,
+                                      ItemHashTable *unique_itemsets);
 void print_text_rule(AssociationRule rule);
+
+#endif
